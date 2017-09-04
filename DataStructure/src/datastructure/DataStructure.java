6f41324f14664d82e55fea8de4ccf8be74b38442
Mario Alberto Vàzquez Lòpez <marioal.vazquez@gmail.com>
2017-09-04 17:11:31 -0500

    cleared obsolete classes

--- DataStructure/src/datastructure/DataStructure.java
+++ DataStructure/src/datastructure/DataStructure.java
@@ -4,8 +4,12 @@
  */
 package datastructure;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Vector;
 import javax.swing.JOptionPane;
 
 /**
@@ -18,8 +22,115 @@ public class DataStructure {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int array [] = {1,2,31,4,25,1};
-        PrintMayMin(array);
+        GetFash();
+    }
+    
+    public static void GetMean()
+    {
+        Random random = new Random();
+        int begining = 100;
+        int end = 999;
+        int mean;
+        int total = 0;
+        int [] array = new int[100];
+                
+        for (int i = 0; i < 100; i++) {
+            int temp = (random.nextInt(end - begining)+ begining);
+            array[i] = temp;
+            if (i == 0) System.out.println("Grupo 1:");
+            if (i == 25) System.out.println("\nGrupo 2:");
+            if (i == 50) System.out.println("\nGrupo 3:");
+            if (i == 75) System.out.println("\nGrupo 4:");
+            System.out.print(temp + ", ");
+            total += temp;
+        }
+        System.out.println("\nPromedio: " + total/100);
+    }
+    
+    public static void GetFash(){
+      Random random = new Random();
+        int begining = 100;
+        int end = 999;
+        int mean;
+        int total = 0;
+        int [] array = new int[100];
+                
+        for (int i = 0; i < 100; i++) {
+            int temp = (random.nextInt(end - begining)+ begining);
+            array[i] = temp;
+            if (i == 0) System.out.println("Grupo 1:");
+            if (i == 25) System.out.println("\nGrupo 2:");
+            if (i == 50) System.out.println("\nGrupo 3:");
+            if (i == 75) System.out.println("\nGrupo 4:");
+            System.out.print(temp + ", ");
+            total += temp;
+        }
+        
+        Arrays.sort(array);
+        
+        int mean1 = 0;
+        int mean2 = 0;
+        int fash1;
+        int fash2;
+        for (int i = 0; i < array.length; i++) {
+            int temp = array[i];
+            if (i <= 99) {
+                if (temp == array[i+1]) {
+                    fash1 = temp;
+                    mean1++;
+                }
+                else{
+                    fash2 = array[i+1];
+                    mean2++;
+                }
+                }
+            }
+        }
+        
+    
+    
+    
+    
+    public static void Vectors()
+    {
+        Vector v = new Vector();
+        String output = "El vector v:\n" + howMany(v);
+        
+        v.add("Rojo");
+        v.add("Verde");
+        v.add("Rosa");
+        v.insertElementAt("Azul", 0);
+        v.insertElementAt("Mango", 1);
+        v.insertElementAt("Amarillo", 3);
+        
+        output += "\n v.toString(): " + v.toString();
+        v.remove("Mango");
+        output += "\n Después de borrar Mango " + v.toString();
+        output += "\n El vector v:\n" + howMany(v);
+        JOptionPane.showMessageDialog(null, output, "Clase Vector", JOptionPane.PLAIN_MESSAGE);
+    }
+    
+    private static String howMany(Vector v) {
+        if (!v.isEmpty()) {
+            return "tiene una capacidad de " + v.capacity() + "\n" + 
+                    "y están ocupados" + v.size();
+        }
+        return "No tiene elementos";
+    }
+    
+    
+    
+    public static void PrintRandom()
+    {
+        final int begining = 12;
+        final int end = 25;
+        
+        Random random = new Random();
+        
+        for (int i = begining; i < end; i++) {
+            System.out.println("Número Aleatorio: " +
+                    (random.nextInt(end - begining)+ begining));
+        }
     }
     
     public static void PrintMayMin(int[] a){
@@ -32,7 +143,8 @@ public class DataStructure {
         System.out.println("El número mayor es: " + may);
         System.out.println("El número mayor es: " + min);
 
-    }public static void SumOfIntegers()
+    }
+    public static void SumOfIntegers()
     {
         String firstNumber;
         String secondNumber;
@@ -97,16 +209,4 @@ public class DataStructure {
         System.out.println("El número menor es: " + men);
     }
     
-    public static void PrintRandom()
-    {
-        final int begining = 12;
-        final int end = 25;
-        
-        Random random = new Random();
-        
-        for (int i = begining; i < end; i++) {
-            System.out.println("Número Aleatorio: " +
-                    (random.nextInt(end - begining)+ begining));
-        }
-    }
-    }
+}
